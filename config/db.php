8ec1c538e4cb25a305285283aaa7528568433b4b
Sergey Mochalov <proweb@users.noreply.github.com>
2025-02-08 00:55:57 +0300

    Add Db

--- config/db.php
+++ config/db.php
@@ -2,13 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'sqlite:@app/db/database.sqlite',
     'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
