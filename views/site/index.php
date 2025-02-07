4fa9292a7ece0a3d75e4833e4c3e0a0e7716b81e
Sergey Mochalov <proweb@users.noreply.github.com>
2025-02-07 19:23:04 +0300

    Update index.php

--- views/site/index.php
+++ views/site/index.php
@@ -11,7 +11,7 @@ $this->title = 'My Yii Application';
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
-        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii in 2025</a></p>
     </div>
 
     <div class="body-content">
